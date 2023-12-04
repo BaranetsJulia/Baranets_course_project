24523c4b4c9d0bc42d1462d6489ac26295441054
BaranetsJulia <baranetsjulia@mail.ru>
2023-12-04 22:51:36 +0500

    Change
    Обновление функционала

--- app/src/main/java/com/example/course_project/MainActivity.java
+++ app/src/main/java/com/example/course_project/MainActivity.java
@@ -2,45 +2,64 @@ package com.example.course_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView myListView;
-    private String [] names = {" ", " "}; //
+    private EditText editTextTask;
+    private Button btnAddTask;
+    private ListView lvTasks;
+    private ArrayAdapter<String> taskAdapter;
+    private ArrayList<String> tasks = new ArrayList<>();
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myListView=(ListView) findViewById(R.id.lv);
-        myListView.setAdapter(new MyBaseAdapter());
-    }
-    class MyBaseAdapter extends BaseAdapter {
-        @Override
-        public int getCount() {
-            return names.length;
-        }
-        @Override
-        public Object getItem(int position) {
-            return names [position];
-        }
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-        @Override
-        public View getView (int position, View convertView, ViewGroup parent) {
-            View view = View.inflate(MainActivity.this, R.layout.list_item, null);
-            TextView mTextView=(TextView) view.findViewById(R.id.tv_list);
-            mTextView.setText(names[position]);
-            return view;
-        }
+        editTextTask = findViewById(R.id.editTextTask);
+        btnAddTask = findViewById(R.id.btnAddTask);
+        lvTasks = findViewById(R.id.lvTasks);
+
+        taskAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasks);
+        lvTasks.setAdapter(taskAdapter);
+
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String task = editTextTask.getText().toString().trim();
+                if (!task.isEmpty()){
+                    tasks.add(task);
+                    taskAdapter.notifyDataSetChanged();
+                    editTextTask.setText("");
+                }
+            }
+        });
+        lvTasks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String task = taskAdapter.getItem(position);
+                Toast.makeText(MainActivity.this, task, Toast.LENGTH_SHORT).show();
+            }
+        });
+        lvTasks.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                tasks.remove(position);
+                taskAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 }
